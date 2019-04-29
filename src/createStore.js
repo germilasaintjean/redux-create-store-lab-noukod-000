e80f12764659dcbb00831aa881d39a991d8c1514
Saint-Jean Germila <germila@noukod.com>
2019-04-29 17:50:21 +0000

    Automatically backed up by Learn

--- src/createStore.js
+++ src/createStore.js
@@ -1,6 +1,21 @@
 export default function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = changeCount(state, action);
+    render();
+    }
+
+    function getState() {
+      return state;
+    }
+
+    return {
+      dispatch,
+      getState
+    };
   // add your code here
-}
+;
 
 function render() {
   const container = document.getElementById('container');
